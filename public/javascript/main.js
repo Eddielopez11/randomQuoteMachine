6255493e902de92f282497e2f51597ecb6d59d29
Eduardo Lopez <eddielopez2101@gmail.com>
2017-06-13 17:59:36 -0500

    testing new api

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
   var xhr = new XMLHttpRequest();
 
-  xhr.open('GET', 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1', true);
+  xhr.open('GET', 'https://talaikis.com/api/quotes/random/', true);
   xhr.onreadystatechange = function() {
     if (xhr.readyState === 4) {
       console.log(xhr.responseText);
@@ -9,4 +9,17 @@ document.addEventListener("DOMContentLoaded", function() {
   };
   xhr.setRequestHeader('Accept', 'application/json');
   xhr.send();
+
+  document.getElementById('newQuote').onclick = function newQuote () {
+    var xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'https://quotesondesign.com/wp-json/filter[id]=1111', true);
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4) {
+        console.log(xhr.responseText);
+      }
+    };
+    xhr.setRequestHeader('Accept', 'application/json');
+    xhr.send();
+  };
 });
