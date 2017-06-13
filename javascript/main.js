f1d66c2b0434c9c1b16d289d3f1c1c5a1835da1f
Eduardo Lopez <eddielopez2101@gmail.com>
2017-06-13 16:24:46 -0500

    added some js scripts

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
-  
+  var xhr = new XMLHttpRequest();
+
+  xhr.open('GET', 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1', true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4) {
+      console.log(xhr.responseText);
+    }
+  };
+  xhr.setRequestHeader('Accept', 'application/json');
+  xhr.send();
 });
