8f15443f6affed4c1e37ec161bd58758143c2f6f
Eduardo Lopez <eddielopez2101@gmail.com>
2017-06-21 11:12:16 -0500

    added functionality to tweet button to auto populate tweet with quote and author

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,7 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
-  var currentQuote;
   var quoteEl = document.querySelector("[data-js='quote']");
   var authorEl = document.querySelector("[data-js='author']");
+  var a = document.querySelector("[data-js='tweetLink']");
+  var currentQuote;
+  var quote;
+  var author;
 
   function getNewQuote() {
     var xhr = new XMLHttpRequest();
@@ -10,12 +13,28 @@ document.addEventListener("DOMContentLoaded", function() {
     xhr.onreadystatechange = function() {
       if (xhr.readyState === 4) {
         currentQuote = JSON.parse(xhr.responseText);
-        quoteEl.innerHTML = currentQuote.quote;
-        authorEl.innerHTML = currentQuote.author;
+        quote = currentQuote.quote;
+        author = currentQuote.author;
+        quoteEl.innerHTML = quote;
+        authorEl.innerHTML = author;
+
+        function URLified() {
+          var twitterLink = "https://twitter.com/intent/tweet?text=";
+
+          var quoteURL = quote.split(" ").join("%20") + "%20-%20" + author.split(" ").join("%20");
+
+          var tweetPopulate = twitterLink + quoteURL;
+
+          return tweetPopulate;
+        };
+        a.href = URLified();
+
       }
     };
     xhr.setRequestHeader('Accept', 'application/json');
     xhr.send();
+
+
   };
 
   window.onload = function() {
@@ -24,7 +43,6 @@ document.addEventListener("DOMContentLoaded", function() {
 
   document.getElementById('newQuote').onclick = function newQuote () {
     getNewQuote();
-
   };
 
 });
